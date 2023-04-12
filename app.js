ca223e487311454774f968813e29be205b8caace
oleg <olegvraby@gmail.com>
2023-04-12 12:00:45 +0200

    added mongoous and created the new DB userDB and got request from register page

--- app.js
+++ app.js
@@ -2,15 +2,41 @@
 const express = require('express')
 const ejs = require('ejs')
 const bodyParser = require('body-parser')
-const path = require('path');
+const mongoose = require('mongoose')
 
 const app = express()
+
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }))
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+mongoose.connect('mongodb://127.0.0.1:27017/userDB');
 
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+})
 
+const User = new mongoose.model("User", userSchema)
+
+app.get('/', (req,res)=>{
+    res.render('home')
+});
+
+app.get('/login', (req,res)=>{
+    res.render('login')
+});
+
+app.get('/register', (req,res)=>{
+    res.render('register')
+});
+
+app.post('/register',(req,res)=>{
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    })
+
+})
 
 
 app.listen(3000,()=>{
