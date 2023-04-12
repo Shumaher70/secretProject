a6932bea07154abad0ac50b965b569fa2883e196
oleg <olegvraby@gmail.com>
2023-04-12 13:17:18 +0200

    added mango-encrypt and encrypted the user's password

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 //jshint esversion:6
-const express = require('express')
-const ejs = require('ejs')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
+const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
 
 const app = express()
 
@@ -16,7 +17,12 @@ const userSchema = new mongoose.Schema({
     password: String
 })
 
+const secret = 'Thisisourlittlesecret.' 
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+
+
 const User = new mongoose.model("User", userSchema)
+
 
 app.get('/', (req,res)=>{
     res.render('home')
@@ -38,7 +44,20 @@ app.post('/register',(req,res)=>{
     newUser.save()
     .then(res.render('secrets'))
     .catch('sometghing wrong, file is not saved')
+});
 
+app.post('/login', (req,res)=>{
+    const username = req.body.username
+    const password = req.body.password
+
+    User.findOne({email: username})
+    .then(foundUser=>{
+        if(foundUser.email == username && foundUser.password == password){
+            res.render('secrets')
+        } else{
+            res.send('your email does not exist or the password is incorrect.')
+        }
+    })
 })
 
 
